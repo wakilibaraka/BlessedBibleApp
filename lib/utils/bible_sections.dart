1a3f68cd8a0d32520ae9f2422067fc03d78cc23d
Emmanuel Baraka Ongau <baraka@Emmanuels-Air.home>
2026-07-27 16:54:23 +0200

    feat: refine biblical navigation picker color-coding with granular groupings

--- lib/utils/bible_sections.dart
+++ lib/utils/bible_sections.dart
@@ -5,10 +5,16 @@ enum BibleSection {
   historical,
   wisdom,
   majorProphets,
-  minorProphets,
+  minorProphetsPreExilic,
+  minorProphetsPostExilic,
   gospels,
   acts,
-  epistles,
+  epistlesPauline,
+  epistlesPetrine,
+  epistlesJohannine,
+  epistleHebrews,
+  epistleJames,
+  epistleJude,
   revelation,
 }
 
@@ -40,44 +46,44 @@ const Map<String, BibleSection> _bookSections = {
   'Lamentations': BibleSection.majorProphets,
   'Ezekiel': BibleSection.majorProphets,
   'Daniel': BibleSection.majorProphets,
-  'Hosea': BibleSection.minorProphets,
-  'Joel': BibleSection.minorProphets,
-  'Amos': BibleSection.minorProphets,
-  'Obadiah': BibleSection.minorProphets,
-  'Jonah': BibleSection.minorProphets,
-  'Micah': BibleSection.minorProphets,
-  'Nahum': BibleSection.minorProphets,
-  'Habakkuk': BibleSection.minorProphets,
-  'Zephaniah': BibleSection.minorProphets,
-  'Haggai': BibleSection.minorProphets,
-  'Zechariah': BibleSection.minorProphets,
-  'Malachi': BibleSection.minorProphets,
+  'Hosea': BibleSection.minorProphetsPreExilic,
+  'Joel': BibleSection.minorProphetsPreExilic,
+  'Amos': BibleSection.minorProphetsPreExilic,
+  'Obadiah': BibleSection.minorProphetsPreExilic,
+  'Jonah': BibleSection.minorProphetsPreExilic,
+  'Micah': BibleSection.minorProphetsPreExilic,
+  'Nahum': BibleSection.minorProphetsPreExilic,
+  'Habakkuk': BibleSection.minorProphetsPreExilic,
+  'Zephaniah': BibleSection.minorProphetsPreExilic,
+  'Haggai': BibleSection.minorProphetsPostExilic,
+  'Zechariah': BibleSection.minorProphetsPostExilic,
+  'Malachi': BibleSection.minorProphetsPostExilic,
   'Matthew': BibleSection.gospels,
   'Mark': BibleSection.gospels,
   'Luke': BibleSection.gospels,
   'John': BibleSection.gospels,
   'Acts': BibleSection.acts,
-  'Romans': BibleSection.epistles,
-  '1 Corinthians': BibleSection.epistles,
-  '2 Corinthians': BibleSection.epistles,
-  'Galatians': BibleSection.epistles,
-  'Ephesians': BibleSection.epistles,
-  'Philippians': BibleSection.epistles,
-  'Colossians': BibleSection.epistles,
-  '1 Thessalonians': BibleSection.epistles,
-  '2 Thessalonians': BibleSection.epistles,
-  '1 Timothy': BibleSection.epistles,
-  '2 Timothy': BibleSection.epistles,
-  'Titus': BibleSection.epistles,
-  'Philemon': BibleSection.epistles,
-  'Hebrews': BibleSection.epistles,
-  'James': BibleSection.epistles,
-  '1 Peter': BibleSection.epistles,
-  '2 Peter': BibleSection.epistles,
-  '1 John': BibleSection.epistles,
-  '2 John': BibleSection.epistles,
-  '3 John': BibleSection.epistles,
-  'Jude': BibleSection.epistles,
+  'Romans': BibleSection.epistlesPauline,
+  '1 Corinthians': BibleSection.epistlesPauline,
+  '2 Corinthians': BibleSection.epistlesPauline,
+  'Galatians': BibleSection.epistlesPauline,
+  'Ephesians': BibleSection.epistlesPauline,
+  'Philippians': BibleSection.epistlesPauline,
+  'Colossians': BibleSection.epistlesPauline,
+  '1 Thessalonians': BibleSection.epistlesPauline,
+  '2 Thessalonians': BibleSection.epistlesPauline,
+  '1 Timothy': BibleSection.epistlesPauline,
+  '2 Timothy': BibleSection.epistlesPauline,
+  'Titus': BibleSection.epistlesPauline,
+  'Philemon': BibleSection.epistlesPauline,
+  'Hebrews': BibleSection.epistleHebrews,
+  'James': BibleSection.epistleJames,
+  '1 Peter': BibleSection.epistlesPetrine,
+  '2 Peter': BibleSection.epistlesPetrine,
+  '1 John': BibleSection.epistlesJohannine,
+  '2 John': BibleSection.epistlesJohannine,
+  '3 John': BibleSection.epistlesJohannine,
+  'Jude': BibleSection.epistleJude,
   'Revelation': BibleSection.revelation,
 };
 
@@ -90,10 +96,16 @@ Color getSectionColor(String bookName, bool isDark) {
     case BibleSection.historical: return isDark ? const Color(0xFF323632) : const Color(0xFFE9F0E9);
     case BibleSection.wisdom: return isDark ? const Color(0xFF36323B) : const Color(0xFFECE7F1);
     case BibleSection.majorProphets: return isDark ? const Color(0xFF3A2D2D) : const Color(0xFFF3E5E5);
-    case BibleSection.minorProphets: return isDark ? const Color(0xFF383129) : const Color(0xFFF1E9E1);
+    case BibleSection.minorProphetsPreExilic: return isDark ? const Color(0xFF383129) : const Color(0xFFF1E9E1);
+    case BibleSection.minorProphetsPostExilic: return isDark ? const Color(0xFF353328) : const Color(0xFFF0EBE0);
     case BibleSection.gospels: return isDark ? const Color(0xFF3C3322) : const Color(0xFFF6EEE0);
     case BibleSection.acts: return isDark ? const Color(0xFF2A343A) : const Color(0xFFE4EDF1);
-    case BibleSection.epistles: return isDark ? const Color(0xFF3A362D) : const Color(0xFFF4F0E5);
+    case BibleSection.epistlesPauline: return isDark ? const Color(0xFF3A362D) : const Color(0xFFF4F0E5);
+    case BibleSection.epistlesPetrine: return isDark ? const Color(0xFF2D363A) : const Color(0xFFE5F0F4);
+    case BibleSection.epistlesJohannine: return isDark ? const Color(0xFF362D3A) : const Color(0xFFF0E5F4);
+    case BibleSection.epistleHebrews: return isDark ? const Color(0xFF313A2D) : const Color(0xFFEBF4E5);
+    case BibleSection.epistleJames: return isDark ? const Color(0xFF3A312A) : const Color(0xFFF4EBE0);
+    case BibleSection.epistleJude: return isDark ? const Color(0xFF303032) : const Color(0xFFEAEAEA);
     case BibleSection.revelation: return isDark ? const Color(0xFF3C2A30) : const Color(0xFFF5E3E7);
   }
 }
