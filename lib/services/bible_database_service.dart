f1a54c942ec77fd7ea46ee563b326fbe357c48d5
Emmanuel Baraka <wakilibar@gmail.com>
2026-09-24 23:37:19 +0300

    feat: Add Prophets and Kings commentary, vacuum bible.db, bump db version to 11

--- lib/services/bible_database_service.dart
+++ lib/services/bible_database_service.dart
@@ -24,7 +24,7 @@ class BibleDatabaseService {
 
     final prefs = await SharedPreferences.getInstance();
     final currentDbVersion = prefs.getInt('db_version') ?? 1;
-    const requiredDbVersion = 10;
+    const requiredDbVersion = 11;
 
     final dbFile = File(dbPath);
     final backupFile = File(backupPath);
